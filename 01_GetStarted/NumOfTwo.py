5f47efd2937e4e8ef714c5af4c75a0de99861373
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-03-09 12:46:53 +0530

    feat: added problem 1

---
+++ 01_GetStarted/NumOfTwo.py
@@ -0,0 +1,16 @@
+"""
+Problem : Given two integer numbers, 
+return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+"""
+
+
+# declaring a fuction for problem :
+def multiplyOrSum(n1, n2):
+    if n1 * n2 <= 1000:
+        return n1 * n2
+    else:
+        return n1 + n2
+
+
+print("for 20 , 30 : ", multiplyOrSum(20, 30))  # it will return multiply
+print("for 5, 9 : ", multiplyOrSum(1000, 2))  # it will return sum
