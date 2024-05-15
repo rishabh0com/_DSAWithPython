834d485ecd581befbddb4b13a476c4f3ef0f541f
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-05-15 21:22:32 +0530

    added list notes

---
+++ Notes/List.py
@@ -0,0 +1,19 @@
+# List in Python mutable collection of objects
+
+## Creating a List : []
+my_list = [1,2,3,4,5,6];
+
+## Access Element from a List :
+print(my_list[1]) #at 1st index element #2
+
+## Slicing on List :
+# my_List[start : end : step]
+print(my_list[::-1]) # print list in reverse form
+print(my_list[-1:-7:-1]) # reverse
+
+print(True) if 6 in my_list else print(False); # check, element is present in list or not
+
+# List Methods : 
+a = [] # empty list 
+print(help(a)) # list of methods available in list class 
+
